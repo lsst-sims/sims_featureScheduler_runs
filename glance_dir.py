12385e39d844731392084a838dbe44c3e2409367
Peter Yoachim <yoachim@uw.edu>
2019-02-07 15:25:39 -0800

    more readmes

---
+++ glance_dir.py
@@ -0,0 +1,32 @@
+import os
+import glob
+import shutil
+import lsst.sims.maf.batches as batches
+import lsst.sims.maf.db as db
+import lsst.sims.maf.metricBundles as mb
+
+
+if __name__ == "__main__":
+    """
+    Run the glance batch on all .db files in a directory.
+    """
+    db_files = glob.glob('*.db')
+    run_names = [name.replace('.db', '') for name in db_files]
+    if os.path.isfile('trackingDb_sqlite.db'):
+        os.remove('trackingDb_sqlite.db')
+    for name in run_names:
+        if os.path.isdir(name):
+            shutil.rmtree(name)
+        opsdb = db.OpsimDatabaseV4(name+'.db')
+        colmap = batches.ColMapDict('OpsimV4')
+
+        bdict = {}
+        bdict.update(batches.glanceBatch(colmap, name))
+        bdict.update(batches.fOBatch(colmap, name))
+        resultsDb = db.ResultsDb(outDir=name)
+        group = mb.MetricBundleGroup(bdict, opsdb, outDir=name, resultsDb=resultsDb)
+        group.runAll()
+        group.plotAll()
+        resultsDb.close()
+        opsdb.close()
+        db.addRunToDatabase(name, 'trackingDb_sqlite.db', None, name, '', '', name+'.db')
---
+++ stability_test/README.md
@@ -0,0 +1,5 @@
+
+Test how small variations change the final result of the survey.
+
+Can test minor changes in the basis function weights. Can also offset the start of the survey by day, week, month, year.
+
