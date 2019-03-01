5e3f04ec2553299cc5cd89100521ac9a4fd11355
Peter Yoachim <yoachim@uw.edu>
2019-03-01 11:15:46 -0800

    adding a new 1s twilight survey

--- twilight/README.md
+++ twilight/README.md
@@ -1,1 +1,8 @@
 twilight observing
+
+
+Initial example replaces the twilight greedy survey with a 1-second all sky survey
+
+To run some glances with and without the twilight surveys:
+run_glance.py twilight_1s0yrs.db --sqlConstraint "note='twilight'" --outDir twilight
+run_glance.py twilight_1s0yrs.db --sqlConstraint "note!='twilight'" --outDir notwilight
---
+++ twilight/onesec.py
@@ -0,0 +1,135 @@
+import numpy as np
+import matplotlib.pylab as plt
+import healpy as hp
+from lsst.sims.featureScheduler.modelObservatory import Model_observatory
+from lsst.sims.featureScheduler.schedulers import Core_scheduler
+from lsst.sims.featureScheduler.utils import standard_goals, calc_norm_factor
+import lsst.sims.featureScheduler.basis_functions as bf
+from lsst.sims.featureScheduler.surveys import (generate_dd_surveys, Greedy_survey,
+                                                Blob_survey)
+from lsst.sims.featureScheduler import sim_runner
+
+
+def gen_greedy_surveys(nside, nexp=1):
+    """
+    Make a quick set of greedy surveys
+
+
+    """
+    target_map = standard_goals(nside=nside)
+    # Let's just set it to 1 in all filters 
+    for key in target_map:
+        target_map[key][np.where(target_map[key] > 0)] = 1
+    norm_factor = calc_norm_factor(target_map)
+    filters = ['u', 'g', 'r', 'i', 'z', 'y']
+
+    surveys = []
+
+    for filtername in filters:
+        bfs = []
+        bfs.append(bf.M5_diff_basis_function(filtername=filtername, nside=nside))
+        bfs.append(bf.Target_map_basis_function(filtername=filtername,
+                                                target_map=target_map[filtername],
+                                                out_of_bounds_val=np.nan, nside=nside,
+                                                norm_factor=norm_factor))
+        bfs.append(bf.Slewtime_basis_function(filtername=filtername, nside=nside))
+        bfs.append(bf.Strict_filter_basis_function(filtername=filtername))
+        # Masks, give these 0 weight
+        bfs.append(bf.Zenith_shadow_mask_basis_function(nside=nside, shadow_minutes=60., max_alt=76.))
+        bfs.append(bf.Moon_avoidance_basis_function(nside=nside, moon_distance=40.))
+        bfs.append(bf.Clouded_out_basis_function())
+
+        bfs.append(bf.Filter_loaded_basis_function(filternames=filtername))
+
+        weights = np.array([3.0, 0.3, 3., 3., 0., 0., 0., 0.])
+        surveys.append(Greedy_survey(bfs, weights, block_size=1, filtername=filtername,
+                                     dither=True, nside=nside, ignore_obs=['DD', 'blob'], nexp=nexp,
+                                     exptime=1., survey_name='twilight'))
+
+    return surveys
+
+
+def generate_blobs(nside, mixed_pairs=False, nexp=1):
+    target_map = standard_goals(nside=nside)
+    norm_factor = calc_norm_factor(target_map)
+
+    # List to hold all the surveys (for easy plotting later)
+    surveys = []
+
+    # Set up observations to be taken in blocks
+    filter1s = ['u', 'g', 'r', 'i', 'z', 'y']
+    if mixed_pairs:
+        filter2s = [None, 'r', 'i', 'z', None, None]
+    else:
+        filter2s = [None, 'g', 'r', 'i', None, None]
+    # Ideal time between taking pairs
+    pair_time = 22.
+    times_needed = [pair_time, pair_time*2]
+    for filtername, filtername2 in zip(filter1s, filter2s):
+        bfs = []
+        bfs.append(bf.M5_diff_basis_function(filtername=filtername, nside=nside))
+        if filtername2 is not None:
+            bfs.append(bf.M5_diff_basis_function(filtername=filtername2, nside=nside))
+        bfs.append(bf.Target_map_basis_function(filtername=filtername,
+                                                target_map=target_map[filtername],
+                                                out_of_bounds_val=np.nan, nside=nside,
+                                                norm_factor=norm_factor))
+        if filtername2 is not None:
+            bfs.append(bf.Target_map_basis_function(filtername=filtername2,
+                                                    target_map=target_map[filtername2],
+                                                    out_of_bounds_val=np.nan, nside=nside,
+                                                    norm_factor=norm_factor))
+        bfs.append(bf.Slewtime_basis_function(filtername=filtername, nside=nside))
+        bfs.append(bf.Strict_filter_basis_function(filtername=filtername))
+        # Masks, give these 0 weight
+        bfs.append(bf.Zenith_shadow_mask_basis_function(nside=nside, shadow_minutes=60., max_alt=76.))
+        bfs.append(bf.Moon_avoidance_basis_function(nside=nside, moon_distance=30.))
+        bfs.append(bf.Clouded_out_basis_function())
+        filternames = [fn for fn in [filtername, filtername2] if fn is not None]
+        bfs.append(bf.Filter_loaded_basis_function(filternames=filternames))
+        if filtername2 is None:
+            time_needed = times_needed[0]
+        else:
+            time_needed = times_needed[1]
+        bfs.append(bf.Time_to_twilight_basis_function(time_needed=time_needed))
+        bfs.append(bf.Not_twilight_basis_function())
+        weights = np.array([3.0, 3.0, .3, .3, 3., 3., 0., 0., 0., 0., 0., 0.])
+        if filtername2 is None:
+            # Need to scale weights up so filter balancing still works properly.
+            weights = np.array([6.0, 0.6, 3., 3., 0., 0., 0., 0., 0., 0.])
+        if filtername2 is None:
+            survey_name = 'blob, %s' % filtername
+        else:
+            survey_name = 'blob, %s%s' % (filtername, filtername2)
+        surveys.append(Blob_survey(bfs, weights, filtername1=filtername, filtername2=filtername2,
+                                   ideal_pair_time=pair_time, nside=nside,
+                                   survey_note=survey_name, ignore_obs=['DD', 'twilight'], 
+                                   dither=True, nexp=nexp))
+
+    return surveys
+
+
+def run_sched(surveys, survey_length=365.25, nside=32, fileroot='fileroot'):
+    years = np.round(survey_length/365.25)
+    scheduler = Core_scheduler(surveys, nside=nside)
+    n_visit_limit = None
+    observatory = Model_observatory(nside=nside)
+    observatory, scheduler, observations = sim_runner(observatory, scheduler,
+                                                      survey_length=survey_length,
+                                                      filename=fileroot+'%iyrs.db' % years,
+                                                      delete_past=True, n_visit_limit=n_visit_limit)
+
+
+if __name__ == "__main__":
+    nside = 32
+    survey_length = 365.25 #365.25*10  # Days
+
+    nexp = 1
+
+    greedy = gen_greedy_surveys(nside, nexp=nexp)
+    ddfs = generate_dd_surveys(nside=nside, nexp=nexp)
+    blobs = generate_blobs(nside, nexp=nexp, mixed_pairs=True)
+    surveys = [ddfs, blobs, greedy]
+    run_sched(surveys, survey_length=survey_length, fileroot='twilight_1s')
+
+
