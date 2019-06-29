e5abb897e4bc87fe83be951693b41b89271fb303
Peter Yoachim <yoachim@uw.edu>
2019-06-29 15:14:19 -0700

    big run

---
+++ slurm/make_combo.sh
@@ -0,0 +1,16 @@
+cat run_shortt.script            > run_combo.script
+cat run_alt.script          >> run_combo.script
+cat run_footprints.script   >> run_combo.script
+cat run_templates.script      >> run_combo.script
+cat run_atwilight.script    >> run_combo.script
+cat run_presto.script       >> run_combo.script
+cat run_too.script            >> run_combo.script
+# cat run_baselines.script    >> run_combo.script
+cat run_rolling.script      >> run_combo.script
+# cat run_vary_params.script   >> run_combo.script
+cat run_ddf.script          >> run_combo.script
+cat run_rotator.script      >> run_combo.script
+cat run_weather.script       >> run_combo.script
+cat run_expt.script         >> run_combo.script
+cat run_filterchange.script >> run_combo.script
+cat run_stability.script      >> run_combo.script
